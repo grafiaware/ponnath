570667e67ad710c972a709745c79bc7009957c9b
kasl2405 <kasl2405@grafika-12.intranet.grafia.cz>
2025-04-14 09:47:09 +0200

    další stránky a patička

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -14,8 +14,11 @@ return array(
     'Psr\\Http\\Server\\' => array($vendorDir . '/psr/http-server-handler/src', $vendorDir . '/psr/http-server-middleware/src'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
     'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'Pes\\' => array($vendorDir . '/pes/pes/src'),
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Controler\\' => array($baseDir . '/src/Controler'),
     '' => array($baseDir . '/src'),
 );
