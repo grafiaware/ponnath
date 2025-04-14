72b6966caa913a62ac80c2b60f056b1682590f91
kasl2405 <kasl2405@grafika-12.intranet.grafia.cz>
2025-04-14 16:12:50 +0200

    styly

--- local/templates/body/paticka.php
+++ local/templates/body/paticka.php
@@ -1,12 +1,12 @@
 <footer>
     <div class="ui divider"></div>
     <div class="ui container">
-        <div class="ui grid stackable centered equal width">
+        <div class="ui grid stackable equal width">
             <div class="three wide column">
-                <a href=""><img class="ui medium image" src="<?= $logoImages.'PonnathFamilyGroup_color.webp'?>" alt="logo Ponnath" height=""/></a>
+                <a href=""><img class="ui small image" src="<?= $logoImages.'PonnathFamilyGroup_color.webp'?>" alt="logo Ponnath" height=""/></a>
             </div>
             <div class="column middle aligned">
-                <div class="ui six column grid stackable center aligned">
+                <div class="ui six column compact grid stackable">
                     <div class="column"><a href="page/kontakt">Kontakt</a></div>
                     <div class="column"><a href="page/gdpr">GDPR</a></div>
                     <div class="column"><a href="page/vzor">Vzor</a></div>
